c802cc78791fda0f9ae7055f9eca8925de1b6184
Logic-Voyager <shrutisharma.m31@gmail.com>
2025-04-13 08:47:31 +0530

    changes about css

--- app.py
+++ app.py
@@ -30,8 +30,14 @@ with app.app_context():
     db.create_all()
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    return render_template('home.html')
+
+# @app.route('/home')
+# @login_required
+# def home():
+#     return render_template('home.html')
+
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -58,7 +64,7 @@ def login():
         if user and check_password_hash(user.password, password):
             login_user(user)
             flash('Login successful', 'success')
-            return redirect(url_for('crimes'))
+            return redirect(url_for('home'))
         flash('Invalid credentials', 'danger')
     return render_template('login.html')
 
@@ -117,6 +123,40 @@ def delete_crime(crime_id):
     db.session.commit()
     flash("Crime record deleted successfully.", "success")
     return redirect(url_for('crimes'))
+
+
+
+@app.route('/search_filter_crimes', methods=['GET'])
+def search_filter_crimes():
+    search = request.args.get('search', '')
+    start_date = request.args.get('start_date', '')
+    end_date = request.args.get('end_date', '')
+    crime_type = request.args.get('crime_type', '')
+    location = request.args.get('location', '')
+    
+    # Build query dynamically based on filters
+    query = Crime.query
+    
+    if search:
+        query = query.filter(Crime.description.ilike(f'%{search}%') | Crime.location.ilike(f'%{search}%'))
+    
+    if start_date:
+        query = query.filter(Crime.date >= start_date)
+    
+    if end_date:
+        query = query.filter(Crime.date <= end_date)
+    
+    if crime_type:
+        query = query.filter(Crime.crime_type == crime_type)
+    
+    if location:
+        query = query.filter(Crime.location.ilike(f'%{location}%'))
+
+    # Paginate the results
+    page = request.args.get('page', 1, type=int)
+    crimes = query.paginate(page=page, per_page=10, error_out=False)
+
+    return render_template('search_filter_crimes.html', crimes=crimes)
 
 
 if __name__ == '__main__':
